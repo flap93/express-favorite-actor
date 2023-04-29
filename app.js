920e62153e70de36fd3d62a49fb01b53832b1b2d
Felix Palacios <flpalacios93@gmail.com>
2023-04-29 18:18:52 -0500

    still trying to fix the problem

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 app.get("/about", (req, res, next) => {
-  res.sendFile(__dirname + "/about.html");
+  res.sendFile(__dirname + "/views/about.html");
 });
 
 app.listen(3000);
