1159a5ce1248b40eae0684aed879a45bee2abadf
Felix Palacios <flpalacios93@gmail.com>
2023-04-29 18:20:30 -0500

    fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/views/home.html");
 });
 app.get("/about", (req, res, next) => {
   res.sendFile(__dirname + "/views/about.html");
