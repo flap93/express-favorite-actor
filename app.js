1dc3b96ba67b57ed37d66ae094caffd52a40ef91
Felix Palacios <flpalacios93@gmail.com>
2023-04-29 16:33:49 -0500

    set up navbar

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+app.get("/about", (req, res, next) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.listen(3000);
